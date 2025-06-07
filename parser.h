4d417d04c174f49e7efd756bf9b7b29033972ed6
Douglas Alves <rachebartm00s@proton.me>
2025-06-07 17:56:48 +0100

    added some functions in the libft read man strcspn, strspn, strtok.

--- parser.h
+++ parser.h
@@ -6,20 +6,13 @@
 /*   By: dopereir <dopereir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 23:08:53 by dopereir          #+#    #+#             */
-/*   Updated: 2025/05/30 20:19:42 by dopereir         ###   ########.fr       */
+/*   Updated: 2025/06/07 16:40:57 by dopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 # include "minishell.h"
-
-typedef enum e_command_type
-{
-	CMD_SIMPLE,		// Single command like "ls", "echo", "pwd", "touch"
-	CMD_SEQUENCE,	// commands connected by &&, ||, ;
-	CMD_PIPELINE	// commands connected by |
-}			t_command_type;
 
 typedef struct s_redirect
 {
@@ -28,36 +21,16 @@ typedef struct s_redirect
 	int				fd;	// File descriptor for the redirection	
 }			t_redirect;
 
-typedef struct s_simple_cmd
-{
-	char		**argv; //test.txt 
-	t_redirect	*redirects;		// Array of redirections
-	//int			redirect_count;
-	//bool		background;		// True if & was specified
-}			t_simple_cmd;
-
-typedef struct s_cmd_sequence
-{
-	struct s_command	**commands;	// Array of commands
-	t_token_type		*operators;	// Array of &&, ||, ; between them
-	int					count;
-}			t_cmd_sequence;
-
-typedef struct s_pipeline
-{
-	struct s_command	**commands;	// Array of simple commands
-	int					count;
-}			t_pipeline;
-
 typedef struct s_command //mkdir test argv[0]
 {
-	t_command_type	type;
-	union {
-		t_simple_cmd	simple;
-		//t_cmd_sequence	sequence;
-		t_pipeline		pipeline;
-		
-	} data;
+	t_token_type		type; // Type of command (e.g., simple, pipeline)
+	char			*name; // Name of the command (e.g., "ls", "echo")
+	char			*path; // Full path to the command executable
+	char			*input_file; // Input file for redirection
+	char			*output_file; // Output file for redirection
+	char			*argv[MAX_ARGS]; // Arguments for the command
+	char			*filename; //path to file redirection
+	pid_t			pid_filename_output;
 }			t_command;
 
 
